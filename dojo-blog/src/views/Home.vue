947bf0781be984f461d5de83ba8bea373f9acf23
Erik Seferi <eseferi@c2r13s1.42wolfsburg.de>
2024-10-12 14:14:22 +0200

    practicing with composition api with starting a dojo blog

---
+++ dojo-blog/src/views/Home.vue
@@ -0,0 +1,25 @@
+
+<template>
+    <div class="home">
+        home
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    setup() {
+        console.log('setup')
+    },
+    created() {
+        console.log('created')
+    },
+    mounted() {
+        console.log('mounted')
+    },
+}
+</script>
+
+<style>
+
+</style>
